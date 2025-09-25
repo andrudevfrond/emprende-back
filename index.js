af9e5ca19a1f18397c5894136553ff78a1844f9f
Andres Galindo <sharlly1329g@gmail.com>
2025-09-25 17:35:18 -0500

    listo para ir a github

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const exportFromAnother = require("./another")
 function requestController(){
     //console.log({global})
     //console.log({dir: __dirname})
-    console.log({http})
+    console.log(`hola que hay de nuevo ${exportFromAnother.name}`)
 }
 // configurar nuestro servidor
 const server = http.createServer(requestController)
