f3c2e494f24bb181b6d622d517d3f04178de6a7b
Andres Galindo <andresgalindo@cbit-online.com>
2025-10-15 11:18:49 -0500

    ajustar configuracion de envio de correos

--- helpers/mailer.js
+++ helpers/mailer.js
@@ -3,7 +3,7 @@ const nodemailer = require('nodemailer')
 const transporter = nodemailer.createTransport({
   host: process.env.SMTP_EMAIL,
   port: process.env.SMTP_PORT,
-  secure: false, // upgrade later with STARTTLS
+  secure: true, // upgrade later with STARTTLS
   auth: {
     user: process.env.EMAIL,
     pass: process.env.EMAIL_PASSWORD,
